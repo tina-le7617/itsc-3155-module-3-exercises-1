5db5b226e2f93e55af4e751b6dd1114b98332df8
Tina Le <Tina.Le7617@gmail.com>
2022-01-28 12:06:31 -0500

    Adding module 3 exercise 1

--- exercise_1.py
+++ exercise_1.py
@@ -1,1 +1,8 @@
-# Testing
+def get_unique_list(numbers):
+    list_set = set(numbers)
+    unique_list = (list(list_set))
+    return unique_list 
+
+my_list = [1, 2, 3, 2, 1, 4, 4, 4, 1, 1, 10, 2, 3, 5]
+unique_list = get_unique_list(my_list)
+print(unique_list)
