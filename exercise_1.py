0ca53de352cb7d9c2d0be20992160260c3e85cf5
Tina Le <Tina.Le7617@gmail.com>
2022-01-28 19:47:07 -0500

    Adding in line comments to explain code

--- exercise_1.py
+++ exercise_1.py
@@ -1,6 +1,13 @@
-def get_unique_list(numbers):
-    list_set = set(numbers)
-    unique_list = (list(list_set))
+'''''
+https://www.geeksforgeeks.org/python-get-unique-values-list/
+'''''
+
+def get_unique_list(num):
+    # Taking a list passed in from the parameters, create a set with those numbers
+    set_num = set(num)
+
+    # Create list using list() method that will convert the set back into a list
+    unique_list = (list(set_num))
     return unique_list 
 
 my_list = [1, 2, 3, 2, 1, 4, 4, 4, 1, 1, 10, 2, 3, 5]
